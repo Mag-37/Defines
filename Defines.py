fd68cd219f04e4e0c571553bf9771eeb0c1c2566
Mag-37 <165717182+Mag-37@users.noreply.github.com>
2024-04-24 00:56:06 +0300

    Add files via upload

---
+++ Defines.py
@@ -0,0 +1,7 @@
+def print_params(instruction):
+    print(instruction, 'part1')
+    print(instruction, 'part2')
+
+print_params('instruction,')
+print_params('instruction,')
+print_params('instruction,')
